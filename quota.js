1106b2755f938a07a064897fe9ad84348ec1d0e4
Tim Cheung <thcheung@gmail.com>
2018-10-18 12:23:56 +0100

    remove api key.

--- quota.js
+++ quota.js
@@ -1,7 +1,7 @@
 //exports.myHandler = function(event, context, callback) {
     const ForexDataClient = require("forex-quotes");
 
-    let client = new ForexDataClient('7azJarfThXHYEySeRRSBg7AilSWvsZQA');
+    let client = new ForexDataClient('<API_KEY>');
     client.quota().then(response => {
         console.log(response);
     })
